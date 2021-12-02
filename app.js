d7d6575dd4d9c0731c263080a0213d3a386cb99d
AlinaSyniavska <alina22syniavska@gmail.com>
2021-12-02 09:57:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(activeSlide = 0) {
+  const sliders = document.querySelectorAll('.slide');
+
+  sliders[activeSlide].classList.add('active');
+
+  for (const slide of sliders) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+
+      slide.classList.add('active');
+    });
+  }
+
+  function clearActiveClasses() {
+    sliders.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
+}
+
+slidesPlugin(2); //при завантаженні активний 3-й слайд
